56d4dd4eccb705c0a15f4f9f3dc6ba7d4604b09c
Ernest Asanov <ern.asanov@yandex.ru>
2018-06-09 00:44:52 +0300

    fixes

--- app/src/main/java/employee/summon/asano/model/AddPerson.kt
+++ app/src/main/java/employee/summon/asano/model/AddPerson.kt
@@ -1,6 +1,6 @@
 package employee.summon.asano.model
 
-class AddPerson(val firstтame: String, val lastтame: String, val patronymic: String?,
+class AddPerson(val firstname: String, val lastname: String, val patronymic: String?,
                 val post: String?, val email: String, val phone: String?, val password: String,
                 val lastActiveTime: String
 )
