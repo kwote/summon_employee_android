8443b32f95c86cd3a72487c740d2d261d28da2c2
Ernest Asanov <asanovernest23@gmail.com>
2018-06-11 21:17:00 +0300

    removed redundant fields from AddSummonRequest

--- app/src/main/java/employee/summon/asano/model/AddSummonRequest.kt
+++ app/src/main/java/employee/summon/asano/model/AddSummonRequest.kt
@@ -1,5 +1,3 @@
 package employee.summon.asano.model
 
-data class AddSummonRequest(val callerId: Int, val targetId: Int,
-                         val requested: String, val responded: String? = null,
-                         val state: Int = 0, val enabled: Boolean = false)
+data class AddSummonRequest(val callerId: Int, val targetId: Int)
